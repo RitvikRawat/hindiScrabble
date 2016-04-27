d9a893a11bbbe39d2abc1ebcd0e2954f6d9c530a
Ritvik <ritvik.rawat1996@gmail.com>
2016-04-27 04:25:15 +0000

    Final Update Penalty.cs

--- Scrabble/Assets/Scripts/Penalty.cs
+++ Scrabble/Assets/Scripts/Penalty.cs
@@ -1,3 +1,9 @@
+//This C# script decides the penalty which is a very important 
+//defensive strategy of the AI control. AI decides the usefulness
+//of a word by using the danger the formed word poses.
+//This ensures that the word played by AI is good at scoring and 
+//also NOT allowing the opponent to score.
+
 using UnityEngine;
 using System.Collections;
 
@@ -18,11 +24,19 @@ public class Penalty : MonoBehaviour {
 	public static double penalty (int l ,int r, int u, int d)
 	{
 		double pen = 0;
+//The penalty score is gradually downscaled as distance from word increases.
 		double[] distance = { 0, 1, 0.75, 0.5, 0.25, 0.10, 0.05, 0.025, 0.025};
+        
+        for(int i=1;i<=8;i++)
+        {
+            distance[i]=distance[i]/2;
+        }
+        
 		int dis = 0;
-//up to down word
+//up to down word orientation
 		if (l == r) {
-			//checking the top
+            
+//checking the top for any dangerous square
 			for (dis=1; (dis<=8)&&(u-dis>=0)&&(u-dis<=15); dis++) {
 				if (Board.powers [u - dis, l] == 3) {
 					pen += 10 * distance [dis];
@@ -37,7 +51,7 @@ public class Penalty : MonoBehaviour {
 					pen += 2 * distance [dis];
 				}
 			}
-			//checking the bottom
+//checking the bottom for any dangerous square
 			for (dis=1; (dis<=8)&&(d+dis>=0)&&(d+dis<=15); dis++) {
 				if (Board.powers [d + dis, l] == 3) {
 					pen += 10 * distance [dis];
@@ -53,7 +67,8 @@ public class Penalty : MonoBehaviour {
 				}
 
 			}
-			//checking the right side
+            
+//checking the right side for any dangerous square
 			for (int i=u; i<=d; i++) {
 				for (dis=1; (dis<=8)&&(l+dis>=0)&&(l+dis<=15); dis++) {
 					if (Board.powers [i, l + dis] == 3) {
@@ -71,7 +86,7 @@ public class Penalty : MonoBehaviour {
 				}
 			}
 
-			//checking the left side
+//checking the left side for any dangerous square
 			for (int i=u; i<=d; i++) {
 				for (dis=1; (dis<=8)&&(l-dis>=0)&&(l-dis<=15); dis++) {
 					if (Board.powers [i, l - dis] == 3) {
@@ -90,10 +105,10 @@ public class Penalty : MonoBehaviour {
 			}
 		
 		} 
-//left to right word
+//left to right word orientation
 		else 
 		{
-			//checking the left side
+//checking the left side for any dangerous square
 			for(dis=1;(dis<=8)&&(l-dis>=0)&&(l-dis<=15);dis++)
 			{
 				if (Board.powers [u ,l-dis] == 3) {
@@ -109,7 +124,7 @@ public class Penalty : MonoBehaviour {
 					pen += 2 * distance [dis];
 				}
 			}
-			//checking the right side
+//checking the right side for any dangerous square
 			for(dis=1;(dis<=8)&&(r+dis>=0)&&(r+dis<=15);dis++)
 			{
 				if (Board.powers [u ,r+dis] == 3) {
@@ -125,7 +140,7 @@ public class Penalty : MonoBehaviour {
 					pen += 2 * distance [dis];
 				}
 			}
-			//checking the top
+//checking the top for any dangerous square
 			for(int i=l;i<=r;i++)
 			{
 				for(dis=1;(dis<=8)&&(u-dis>=0)&&(u-dis<=15);dis++)
@@ -144,7 +159,7 @@ public class Penalty : MonoBehaviour {
 					}
 				}
 			}
-			//checking the bottom
+//checking the bottom for any dangerous square
 			for(int i=l;i<=r;i++)
 			{
 				for(dis=1;(dis<=8)&&(u+dis>=0)&&(u+dis<=15);dis++)
